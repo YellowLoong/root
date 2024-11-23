0baa115fc60b3e1b52ccd15ec97f8f0deaa88626
Vu Hoang Long <minhculong2509@gmail.com>
2024-11-24 02:02:50 +0700

    dmm

--- main.js
+++ main.js
@@ -1,5 +1,112 @@
-import { cardList } from "./data/advanced_card_list.js";
-import Advanced_card_vagabond from '../modal/advanced_card_vagabond.js';
+class Advanced_card {
+    constructor(name, imgUrl, influence) {
+        this.name = name;
+        this.imgUrl = imgUrl;
+        this.influence = influence;
+    }
+
+    getName() {
+        return this.name;
+    }
+
+    // Setter cho 'name'
+    setName(newName) {
+        this.name = newName;
+    }
+
+    // Getter cho 'imgUrl'
+    getImgUrl() {
+        return this.imgUrl;
+    }
+
+    // Setter cho 'imgUrl'
+    setImgUrl(newImgUrl) {
+        this.imgUrl = newImgUrl;
+    }
+
+    // Getter cho 'influence'
+    getInfluence() {
+        return this.influence;
+    }
+
+    // Setter cho 'influence'
+    setInfluence(newInfluence) {
+        this.influence = newInfluence;
+    }
+
+    isHugeInfluence() {
+        if (this.influence > 6) {
+            return true;
+        }
+        return false;
+    }
+}
+
+class Advanced_card_vagabond extends Advanced_card {
+    static characterList = [
+        {
+            name: "Thief",
+            imgUrl: "https://ledergames.com/cdn/shop/files/The-Vagabond_6206fe8d-03e4-413a-a1cd-490979470aca.png?v=1635211915"
+        },
+        {
+            name: "Tinker",
+            imgUrl: "https://cf.geekdo-images.com/ePd-9iKLjnqNtDDPBsRjrA__imagepage/img/jG5jsdDN3unrHEjjMy8_ooLkkQs=/fit-in/900x600/filters:no_upscale():strip_icc()/pic3810244.jpg"
+        },
+        {
+            name: "Ranger",
+            imgUrl: "https://i.kickstarter.com/assets/018/930/231/91ee641373ee11b01bbc7cd940c1e61c_original.jpg?fit=scale-down&origin=ugc&q=50&v=1509034069&width=700&sig=R7a7Bc8IGo%2BpJptrSEFVcfl7w6WSl9MJx%2FfCkcX07EM%3D"
+        },
+        {
+            name: "Vagrant",
+            imgUrl: "https://i.pinimg.com/control2/736x/04/90/86/0490863e991c30f89425f3cb60802318.jpg"
+        },
+        {
+            name: "Arbiter",/* x */
+            imgUrl: "https://i.pinimg.com/control2/736x/1f/ae/eb/1faeeba2273c903c134a69e9ac3229e6.jpg"
+        },
+        {
+            name: "Scoundrel",
+            imgUrl: "https://i.pinimg.com/736x/93/0a/9e/930a9e215516dbe66179a63665b8f1fa.jpg"
+        },
+        {
+            name: "Adventurer",
+            imgUrl: "https://ledergames.com/cdn/shop/files/RootVagabondPack_Adventurer.png?v=1647536487&width=1080"
+        },
+        {
+            name: "Ronin",
+            imgUrl: "https://ledergames.com/cdn/shop/files/RootVagabondPack_Ronin.png?v=1647536487&width=1080"
+        },
+        {
+            name: "Harrier",
+            imgUrl: "https://ledergames.com/cdn/shop/files/RootVagabondPack_Harrier.png?v=1647536487&width=1080"
+        },
+    ];
+
+    constructor(name, imgUrl, influence, index, charId) {
+        super(name, imgUrl, influence);
+        this.index = index;
+        this.character = Advanced_card_vagabond.characterList[charId];
+    }
+}
+
+
+var vagaId_1 = Math.floor(Math.random() * 9);
+
+do {
+    var vagaId_2 = Math.floor(Math.random() * 9);
+} while (vagaId_1 == vagaId_2)
+
+const cardList = [
+    new Advanced_card("Eyrie Dynasties", "https://ledercardcdn.seiyria.com/cards/root/en-US/adset-setup-eyrie.webp", 7),
+    new Advanced_card("Marquise de Cat", "https://ledercardcdn.seiyria.com/cards/root/en-US/adset-setup-marquise.webp", 10),
+    new Advanced_card("Woodland Alliance", "https://ledercardcdn.seiyria.com/cards/root/en-US/adset-setup-woodland.webp", 3),
+    new Advanced_card_vagabond("Vagabond", "https://ledercardcdn.seiyria.com/cards/root/en-US/adset-setup-vagabond.webp", 2.5, 1, vagaId_1),
+    new Advanced_card_vagabond("Vagabond", "https://ledercardcdn.seiyria.com/cards/root/en-US/adset-setup-vagabond.webp", 2.5, 2, vagaId_2),
+    new Advanced_card("Riverfolk Company", "https://ledercardcdn.seiyria.com/cards/root/en-US/adset-setup-riverfolk.webp", 5),
+    new Advanced_card("Lizard Cult", "https://ledercardcdn.seiyria.com/cards/root/en-US/adset-setup-cult.webp", 2),
+]
+
+
 
 var adsetBtn = document.querySelector(".adset_btn");
 var adsetCardEles = document.querySelectorAll(".adset_list li");
@@ -37,7 +144,7 @@ function randomPool(num) {
         );
         poolList[gacha] = cardList[gacha];
     }
-    
+
     // console.log(poolList);
     var cnt = 1;
     Object.entries(poolList).forEach(function ([key, value]) {
